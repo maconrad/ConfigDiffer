6c197632640a2065c6407e201609b3bb59472bf2
maconrad <mario.conrad@gmail.com>
2018-10-29 15:18:24 +0100

    Rename

--- mainapp/config.py
+++ mainapp/config.py
@@ -5,13 +5,15 @@ VERSION = "v1"
 #General Definitions
 ROUTER_TEMPLATE = "../Files/Templates/router_eigrp_templ.json"
 INTERFACE_TEMPLATE = "../Files/Templates/interface_templ.json"
-RADIUS_TEMPLATE = "../Files/Templates/radius_templ.json"
+RADIUS_TEMPLATE = "../Files/Templates/radius_all_templ.json"
+DEVICE_SENSOR_TEMPLATE = "../Files/Templates/device_sensor_templ.json"
 
 #Templates to Verify
 TEMPLATES = [
     ROUTER_TEMPLATE,
     INTERFACE_TEMPLATE,
-    RADIUS_TEMPLATE
+    RADIUS_TEMPLATE,
+    DEVICE_SENSOR_TEMPLATE
 ]
 
 #USER Config
--- mainapp/main.py
+++ mainapp/main.py
@@ -1,4 +1,5 @@
 from ciscoconfparse import CiscoConfParse
+from colorama import Fore, Style
 import config
 import json
 
@@ -15,51 +16,58 @@ def main():
 
     for template in config.TEMPLATES:
         template_dict = load_template(template)
-
-        regex_pattern = template_dict["SECTION_REGEX"]
-        sub_regex_patterns = template_dict["LINES"]
         template_name = template_dict["TEMPLATE_NAME"]
         template_type = template_dict["TEMPLATE_TYPE"]
+        print(f'{Fore.BLUE}VERIFYING TEMPlATE: ' + template_name)
+        print(f'**************************************{Style.RESET_ALL}')
 
-        print("VERIFYING TEMPlATE: " + template_name)
+        for template_section in template_dict["SECTIONS"]:
+            regex_pattern = template_section["SECTION_REGEX"]
+            sub_regex_patterns = template_section["LINES"]
+            section_name = template_section["NAME"]
+        #regex_pattern = template_dict["SECTION_REGEX"]
+        #sub_regex_patterns = template_dict["LINES"]
 
-        ## Find all matching sections (multi line objects)
-        for object in confparse.find_objects(regex_pattern):
-            is_valid = True
+            print(f'{Fore.GREEN}-VERIFYING SECTION: {Fore.CYAN}' + section_name + f'{Style.RESET_ALL}')
 
-            ## Mark that some lines are missing if we ever have to print that object
-            cfgdiffs.append_line(" -> MISSING OR DIFFERENTLY CONFIGURED LINES")
-            cfgdiffs.append_line("   " + object.text)
+            ## Find all matching sections (multi line objects)
+            for object in confparse.find_objects(regex_pattern):
+                is_valid = True
 
-            ## Search children of the object
-            for subregex in sub_regex_patterns:
-                if not (object.re_search_children(subregex)):
-                    cfgdiffs.append_line("    " + subregex)
-                    is_valid = False
+                ## Mark that some lines are missing if we ever have to print that object
+                cfgdiffs.append_line(f'{Fore.RED} -> MISSING OR DIFFERENTLY CONFIGURED LINES')
+                cfgdiffs.append_line(f'   ' + object.text + f'{Style.RESET_ALL}')
 
-            if(is_valid==False):
-                for line in cfgdiffs.ioscfg:
-                    print(line)
-            else:
-                print(" -> SUCCESS - CONFIG SECTION: " + template_name + " FOR OBJECT: " + object.text)
+                ## Search children of the object
+                for subregex in sub_regex_patterns:
+                    if not (object.re_search_children(subregex)):
+                        cfgdiffs.append_line("    " + subregex)
+                        is_valid = False
 
-            #Reset cfgdiffs for next object
-            cfgdiffs = CiscoConfParse([])
+                if(is_valid==False):
+                    for line in cfgdiffs.ioscfg:
+                        print(f'{Fore.RED}' + line + f'{Style.RESET_ALL}')
+                else:
+                    print(f'{Fore.GREEN} -> SUCCESS - CONFIG SECTION: ' + template_name + ' FOR OBJECT: ' + object.text + f'{Style.RESET_ALL}')
+
+                #Reset cfgdiffs for next object
+                cfgdiffs = CiscoConfParse([])
 
         ## Find all single line objects
-        if(template_type=='SINGLE_LINE_AND_SECTION'):
+        if(template_type=='SINGLE_LINE_AND_MULTI_SECTION'):
             is_valid = True
+            print(f'{Fore.GREEN}-VERIFYING GENERAL LINES: {Style.RESET_ALL}')
             regex_patterns = template_dict["SINGLE_LINE_REGEXES"]
             for line in regex_patterns:
                 if not confparse.has_line_with(line):
-                    print(" -> MISSING OR DIFFERENTLY CONFIGURED LINES: " + line)
+                    print(f'{Fore.RED} -> MISSING OR DIFFERENTLY CONFIGURED LINES: ' + line + f'{Style.RESET_ALL}')
                     is_valid = False
             if (is_valid==True):
-                print("-> SUCCESS - GENERAL CONFIG PARTS: " + template_name)
+                print(f'{Fore.GREEN} -> SUCCESS - GENERAL CONFIG PARTS: ' + template_name + f'{Style.RESET_ALL}')
+
+        print(f'{Fore.BLUE}*****************END*********************{Style.RESET_ALL}\n')
 
 
 if __name__ == "__main__":
-    # create CiscoConfParse object using a configuration file that is stored in the
-    # same directory as the script
     main()
 
