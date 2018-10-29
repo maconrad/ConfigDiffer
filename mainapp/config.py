ec1d3b6d0da6980a0da0062f67a1ec5edc316496
maconrad <mario.conrad@gmail.com>
2018-10-29 15:26:22 +0100

    Rename

--- mainapp/config.py
+++ mainapp/config.py
@@ -7,13 +7,15 @@ ROUTER_TEMPLATE = "../Files/Templates/router_eigrp_templ.json"
 INTERFACE_TEMPLATE = "../Files/Templates/interface_templ.json"
 RADIUS_TEMPLATE = "../Files/Templates/radius_all_templ.json"
 DEVICE_SENSOR_TEMPLATE = "../Files/Templates/device_sensor_templ.json"
+DOT1X_TEMPLATE = "../Files/Templates/dot1x_general_templ.json"
 
 #Templates to Verify
 TEMPLATES = [
     ROUTER_TEMPLATE,
     INTERFACE_TEMPLATE,
     RADIUS_TEMPLATE,
-    DEVICE_SENSOR_TEMPLATE
+    DEVICE_SENSOR_TEMPLATE,
+    DOT1X_TEMPLATE
 ]
 
 #USER Config
